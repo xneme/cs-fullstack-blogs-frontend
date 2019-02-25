fcc4e04e24cd1a136f4defcf0571b8957e8a17e7
Jere Helén <jere.helen@helsinki.fi>
2019-02-25 10:01:28 +0200

    Fixed ForwardRef clashing with PropTypes

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,7 +1,7 @@
 import React, { useState, useImperativeHandle } from 'react'
 import PropTypes from 'prop-types'
 
-const Togglable = (props, ref) => {
+const Togglable = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
@@ -28,10 +28,10 @@ const Togglable = (props, ref) => {
       </div>
     </div>
   )
-}
+})
 
 Togglable.propTypes = {
   buttonLabel: PropTypes.string.isRequired
 }
 
-export default React.forwardRef(Togglable)
+export default Togglable
